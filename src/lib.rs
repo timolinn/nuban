e2bb9186a836064fcc04f721c7f9b41d8807fb20
timolinn <timothyonyiuke@gmail.com>
2019-03-20 15:42:34 +0100

    implemented calculate_check_digit and added test for get_bank_name

--- src/lib.rs
+++ src/lib.rs
@@ -40,8 +40,8 @@ impl Nuban {
     ///     use nuban::Nuban;
     ///
     ///     fn main() {
-    ///         let nuban = Nuban::new("058", "0739082716");
-    ///         if let nuban.is_valid() = Ok(true) {
+    ///         let nuban = Nuban::new("058", "0739082716").unwrap();
+    ///         if let Ok(true) = nuban.is_valid() {
     ///             println!("'{}' is a valid account number", nuban.account_number());
     ///         } else {
     ///             println!("'{}' is not a valid account number", nuban.account_number());
@@ -49,27 +49,9 @@ impl Nuban {
     ///     }
     /// ```
     pub fn is_valid(&self) -> Result<bool, bool> {
-        let mut account_number = self.account_number.clone();
-        let check_digit = account_number.pop().unwrap();
-        let mut numbers = String::new();
-        numbers.push_str(&self.bank_code);
-        numbers.push_str(&account_number);
+        let check_digit = self.account_number.clone().pop().unwrap();
 
-        // convert str to a list ints to enable calculation
-        let number_ints = numbers.chars().map(|num| num.to_digit(10).unwrap());
-
-        let multipliers = vec![3, 7, 3, 3, 7, 3, 3, 7, 3, 3, 7, 3];
-        let mut check_sum = 0;
-        for (idx, num) in number_ints.enumerate() {
-            check_sum += num * multipliers[idx];
-        }
-
-        let mut correct_check_digit = 10 - (check_sum % 10);
-        if correct_check_digit == 10 {
-            correct_check_digit = 0;
-        }
-
-        if correct_check_digit == check_digit.to_digit(10).unwrap()  {
+        if self.calculate_check_digit().unwrap() == check_digit.to_digit(10).unwrap() as u8 {
             Ok(true)
         } else {
             Err(false)
@@ -85,11 +67,28 @@ impl Nuban {
     }
 
     pub fn calculate_check_digit(&self) -> Result<u8, &str> {
-        Ok(0)
-    }
 
-    pub fn check_digit(&self) -> Result<u8, u8> {
-        Ok(0)
+        let mut account_number = self.account_number.clone();
+        let check_digit = account_number.pop().unwrap();
+
+        let mut numbers = String::new();
+        numbers.push_str(&self.bank_code);
+        numbers.push_str(&account_number);
+
+        // convert str to a list ints to enable calculation
+        let number_ints = numbers.chars().map(|num| num.to_digit(10).unwrap());
+
+        let multipliers = vec![3, 7, 3, 3, 7, 3, 3, 7, 3, 3, 7, 3];
+        let mut check_sum = 0;
+        for (idx, num) in number_ints.enumerate() {
+            check_sum += num * multipliers[idx];
+        }
+
+        let mut correct_check_digit = (10 - (check_sum % 10) as u8);
+        if correct_check_digit == 10 {
+            correct_check_digit = 0;
+        }
+        Ok(correct_check_digit)
     }
 
     fn banks(&self) -> HashMap<&str, &str> {
@@ -104,7 +103,7 @@ impl Nuban {
             ("214", "FCMB"),
             ("070", "Fidelity"),
             ("085", "FinBank"),
-            ("058", "Guarantee Trust Bank"),
+            ("058", "Guaranty Trust Bank"),
             ("069", "Intercontinentl Bank"),
             ("056", "Oceanic Bank"),
             ("082", "BankPhb"),
@@ -129,7 +128,7 @@ impl Nuban {
 mod tests {
     use super::*;
     #[test]
-    fn it_returns_new_nuban_instance() {
+    fn test_returns_new_nuban_instance() {
         let account = Nuban::new("058", "0982736625");
         assert_eq!(
             account.unwrap(),
@@ -141,14 +140,27 @@ mod tests {
     }
 
     #[test]
-    fn it_returns_false_for_invalid_account() {
+    fn test_returns_false_for_invalid_account() {
         let account = Nuban::new("058", "0982736625").unwrap();
         assert!(account.is_valid().is_err());
     }
 
     #[test]
-    fn it_returns_true_for_valid_account() {
+    fn test_returns_true_for_valid_account() {
         let account = Nuban::new("058", "0152792740").unwrap();
         assert!(account.is_valid().is_ok());
+    }
+
+    #[test]
+    fn test_calculate_check_digit() {
+        let account = Nuban::new("058", "0152792740").unwrap();
+        let correct_check_digit = account.calculate_check_digit().unwrap();
+        assert_eq!(correct_check_digit, 0);
+    }
+
+    #[test]
+    fn test_get_bank_name() {
+        let account = Nuban::new("058", "0152792740").unwrap();
+        assert_eq!(account.get_bank_name().unwrap(), String::from("Guaranty Trust Bank"));
     }
 }

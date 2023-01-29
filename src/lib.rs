3ddb72511c3cf7ece2d257ae30cec1bdf0e47b5d
Miraculous Owonubi <omiraculous@gmail.com>
2023-01-29 15:44:41 +0100

    Refactor library and add new methods (#1)
    * plain bool on ::is_valid()
    
    * no vector collection allocation
    
    * refactor calculate_check_digit: i can haz iterators
    
    * refactor ::is_valid()
    
    * refactor calculate_check_digit to make it more readable
    
    * ::banks() should be static method on the struct
    
    * single global static bank map allocation
    
    * impl Eq + Clone for Nuban
    
    * restructure ::new() and ::get_bank_name(): use structured errors
    
    * refactor whole lib, no unnecessary alloc, no invalid Nuban-s
    
    * feature: search account number for matching banks
    
    * bank_matches: remove explicit lifetimes

--- src/lib.rs
+++ src/lib.rs
@@ -1,110 +1,145 @@
 //! This is a lightweight crate for verifying NUBAN numbers
 //! for all Nigerian bank accounts as was directed by the CBN.
 
-use std::collections::HashMap;
+use std::{cell::Cell, collections::HashMap, fmt, sync::Once};
 
-#[derive(PartialEq, Debug)]
-pub struct Nuban {
-    bank_code: String,
-    account_number: String,
+pub const BANKS: [(&'static str, &'static str); 24] = [
+    ("044", "Access Bank"),
+    ("014", "Afribank"),
+    ("023", "Citibank"),
+    ("063", "Diamond Bank"),
+    ("050", "Ecobank"),
+    ("040", "Equitorial Trust Bank"),
+    ("011", "First Bank"),
+    ("214", "FCMB"),
+    ("070", "Fidelity"),
+    ("085", "FinBank"),
+    ("058", "Guaranty Trust Bank"),
+    ("069", "Intercontinentl Bank"),
+    ("056", "Oceanic Bank"),
+    ("082", "BankPhb"),
+    ("076", "Skye Bank"),
+    ("084", "SpringBank"),
+    ("221", "StanbicIBTC"),
+    ("068", "Standard Chartered Bank"),
+    ("232", "Sterling Bank"),
+    ("033", "United Bank For Africa"),
+    ("032", "Union Bank"),
+    ("035", "Wema Bank"),
+    ("057", "Zenith Bank"),
+    ("215", "Unity Bank"),
+];
+
+struct LazyBanks(Once, Cell<Option<HashMap<&'static str, &'static str>>>);
+
+unsafe impl Sync for LazyBanks {}
+
+static LAZY_BANKS: LazyBanks = LazyBanks(Once::new(), Cell::new(None));
+
+#[derive(Eq, Clone, Debug, PartialEq)]
+pub struct Nuban<'a>(&'a str, &'a str, &'a str);
+
+#[derive(Eq, Copy, Clone, Debug, PartialEq)]
+pub enum Error {
+    InvalidBankCode,
+    InvalidAccountNumber,
 }
 
-impl Nuban {
-    pub fn new(bank_code: &str, account_number: &str) -> Result<Self, &'static str> {
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let reason = match self {
+            Error::InvalidBankCode => "invalid bank code",
+            Error::InvalidAccountNumber => "invalid account number",
+        };
+        write!(f, "{}", reason)
+    }
+}
 
-        if bank_code.len() != 3 || account_number.len() != 10 {
-            return Err("Validation Error: invalid bank code or account number");
-        }
+impl<'a> Nuban<'a> {
+    pub fn new(bank_code: &'a str, account_number: &'a str) -> Result<Self, Error> {
+        #[rustfmt::skip] {
+            if !Self::is_valid_bank(bank_code) { Err(Error::InvalidBankCode)? }
+            if account_number.len() != 10  { Err(Error::InvalidAccountNumber)? }
+        };
 
-        Ok(Nuban {
-            bank_code: bank_code.to_string(),
-            account_number: account_number.to_string(),
-        })
+        let (_, check_digit) = Self::account_number_parts(bank_code, account_number)?;
+        Ok(Nuban(bank_code, account_number, check_digit))
     }
 
-    pub fn get_bank_name(&self) -> Result<&str, &str> {
-        let banks = self.banks();
-        let bank_name = banks.get(self.bank_code());
-        match bank_name {
-            Some(_name) => Ok(bank_name.unwrap()),
-            None => Err("Bank not found."),
+    fn account_number_parts(
+        bank_code: &'a str,
+        account_number: &'a str,
+    ) -> Result<(&'a str, &'a str), Error> {
+        let (account_number, check_digit) = account_number.split_at(9);
+        match (
+            check_digit.chars().next().unwrap().to_digit(10),
+            Self::calculate_check_digit(bank_code, account_number),
+        ) {
+            (Some(l), r) if l != r => Err(Error::InvalidAccountNumber),
+            _ => Ok((account_number, check_digit)),
         }
     }
 
-    pub fn is_valid(&self) -> Result<bool, bool> {
-        let check_digit = self.account_number.clone().pop().unwrap();
-
-        if self.calculate_check_digit().unwrap() == check_digit.to_digit(10).unwrap() as u8 {
-            Ok(true)
-        } else {
-            Err(false)
-        }
+    pub fn is_valid_bank(bank_code: &str) -> bool {
+        bank_code.len() == 3 && Self::banks().contains_key(bank_code)
     }
 
-    pub fn account_number(&self) -> &str {
-        &self.account_number
+    pub fn is_valid_account(bank_code: &str, account_number: &str) -> bool {
+        Nuban::new(bank_code, account_number).is_err()
     }
 
     pub fn bank_code(&self) -> &str {
-        &self.bank_code
+        self.0
     }
 
-    pub fn calculate_check_digit(&self) -> Result<u8, &str> {
-
-        let mut account_number = self.account_number.clone();
-        // remove the last char so we can compute the check digit
-        account_number.pop();
-
-        let mut numbers = String::new();
-        numbers.push_str(&self.bank_code);
-        numbers.push_str(&account_number);
-
-        // convert str to a list of ints to enable calculation
-        let number_ints = numbers.chars().map(|num| num.to_digit(10).unwrap());
-
-        let multipliers = vec![3, 7, 3, 3, 7, 3, 3, 7, 3, 3, 7, 3];
-        let mut check_sum = 0;
-        for (idx, num) in number_ints.enumerate() {
-            check_sum += num * multipliers[idx];
-        }
-
-        let mut correct_check_digit = 10 - (check_sum % 10) as u8;
-        if correct_check_digit == 10 {
-            correct_check_digit = 0;
-        }
-        Ok(correct_check_digit)
+    pub fn bank_name(&self) -> &str {
+        Self::banks().get(self.0).unwrap()
     }
 
-    pub fn banks(&self) -> HashMap<&str, &str> {
-        let banks: HashMap<&str, &str> = vec![
-            ("044", "Access Bank"),
-            ("014", "Afribank"),
-            ("023", "Citibank"),
-            ("063", "Diamond Bank"),
-            ("050", "Ecobank"),
-            ("040", "Equitorial Trust Bank"),
-            ("011", "First Bank"),
-            ("214", "FCMB"),
-            ("070", "Fidelity"),
-            ("085", "FinBank"),
-            ("058", "Guaranty Trust Bank"),
-            ("069", "Intercontinentl Bank"),
-            ("056", "Oceanic Bank"),
-            ("082", "BankPhb"),
-            ("076", "Skye Bank"),
-            ("084", "SpringBank"),
-            ("221", "StanbicIBTC"),
-            ("068", "Standard Chartered Bank"),
-            ("232", "Sterling Bank"),
-            ("033", "United Bank For Africa"),
-            ("032", "Union Bank"),
-            ("035", "Wema Bank"),
-            ("057", "Zenith Bank"),
-            ("215", "Unity Bank"),
-        ]
-        .into_iter()
-        .collect();
-        banks
+    pub fn account_number(&self) -> &str {
+        self.1
+    }
+
+    pub fn check_digit(&self) -> &str {
+        self.2
+    }
+
+    fn calculate_check_digit(bank_code: &'a str, account_number: &'a str) -> u32 {
+        // The Approved NUBAN format: [ABC][DEFGHIJKL][M], where
+        //   -       ABC : 3-digit Bank Code
+        //   - DEFGHIJKL : NUBAN Account Serial Number
+        //   -         M : NUBAN Check Digit
+        // https://www.cbn.gov.ng/OUT/2011/CIRCULARS/BSPD/NUBAN%20PROPOSALS%20V%200%204-%2003%2009%202010.PDF
+
+        let nuban_chars = bank_code.chars().chain(account_number.chars());
+        let nuban_digits = nuban_chars.map(|num| num.to_digit(10).unwrap());
+        let seed = [3, 7, 3, 3, 7, 3, 3, 7, 3, 3, 7, 3].iter();
+        let check_sum: u32 = seed.zip(nuban_digits).map(|(l, r)| l * r).sum();
+        match 10 - (check_sum % 10) {
+            10 => 0,
+            x => x,
+        }
+    }
+
+    pub fn banks() -> &'static HashMap<&'static str, &'static str> {
+        LAZY_BANKS
+            .0
+            .call_once(|| LAZY_BANKS.1.set(Some(BANKS.iter().copied().collect())));
+
+        unsafe {
+            if let Some(ref banks) = *LAZY_BANKS.1.as_ptr() {
+                return banks;
+            }
+            unreachable!()
+        }
+    }
+
+    pub fn bank_matches(account_number: &str) -> Result<impl Iterator<Item = (&str, &str)>, Error> {
+        #[rustfmt::skip] if account_number.len() != 10  { Err(Error::InvalidAccountNumber)? };
+        Ok(BANKS
+            .iter()
+            .copied()
+            .filter(move |(code, _)| Self::account_number_parts(code, account_number).is_ok()))
     }
 }
 
@@ -113,38 +148,45 @@ mod tests {
     use super::*;
     #[test]
     fn test_returns_new_nuban_instance() {
-        let account = Nuban::new("058", "0982736625");
-        assert_eq!(
-            account.unwrap(),
-            Nuban {
-                bank_code: String::from("058"),
-                account_number: String::from("0982736625")
-            }
-        );
+        let account = Nuban::new("058", "0152792740");
+        assert_eq!(account.unwrap(), Nuban("058", "0152792740", "0"));
     }
 
     #[test]
     fn test_returns_false_for_invalid_account() {
-        let account = Nuban::new("058", "0982736625").unwrap();
-        assert!(account.is_valid().is_err());
+        let account = Nuban::new("058", "0982736625");
+        assert!(account.is_err());
     }
 
     #[test]
     fn test_returns_true_for_valid_account() {
-        let account = Nuban::new("058", "0152792740").unwrap();
-        assert!(account.is_valid().is_ok());
+        let account = Nuban::new("058", "0152792740");
+        assert!(account.is_ok());
     }
 
     #[test]
     fn test_calculate_check_digit() {
-        let account = Nuban::new("058", "0152792740").unwrap();
-        let correct_check_digit = account.calculate_check_digit().unwrap();
+        let correct_check_digit = Nuban::calculate_check_digit("058", "0152792740");
         assert_eq!(correct_check_digit, 0);
     }
 
     #[test]
     fn test_get_bank_name() {
         let account = Nuban::new("058", "0152792740").unwrap();
-        assert_eq!(account.get_bank_name().unwrap(), String::from("Guaranty Trust Bank"));
+        assert_eq!(account.bank_name(), String::from("Guaranty Trust Bank"));
+    }
+
+    #[test]
+    fn bank_matches() {
+        let matches = Nuban::bank_matches("2209514399")
+            .unwrap()
+            .map(|(code, _)| code)
+            .collect::<Vec<_>>();
+        let map = [
+            "068", // Standard Chartered Bank
+            "035", // Wema Bank
+            "057", // Zenith Bank
+        ];
+        assert!(map.iter().all(|code| matches.contains(code)));
     }
 }
